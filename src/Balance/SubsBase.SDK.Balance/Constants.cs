3967ada77b8a20dbf544613d416aa36d841caf4e
Mohamed Shaker <m.elzantot@gmail.com>
2024-06-11 08:44:33 +0300

    constants has been added

---
+++ src/Balance/SubsBase.SDK.Balance/Constants.cs
@@ -0,0 +1,9 @@
+namespace SubsBase.SDK.Balance;
+
+public static class Constants
+{
+    public const string BalanceUri = @"BaseUrl/balance";
+    public const string BalanceMovementUri = @"BaseUrl/balance/balance-movement";
+    public const string OnHoldAmountUri = @"BaseUrl/balance/on-hold-amount";
+
+}
