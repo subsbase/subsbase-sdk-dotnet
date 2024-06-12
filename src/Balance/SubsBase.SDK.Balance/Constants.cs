67d62c68c1272b3a744a148674a022870aabd8ab
Mohamed Shaker <m.elzantot@gmail.com>
2024-06-12 12:54:30 +0300

    constant params updated t  dev

--- src/Balance/SubsBase.SDK.Balance/Constants.cs
+++ src/Balance/SubsBase.SDK.Balance/Constants.cs
@@ -2,8 +2,8 @@ namespace SubsBase.SDK.Balance;
 
 public static class Constants
 {
-    public const string BalanceUri = @"BaseUrl/balance";
-    public const string BalanceMovementUri = @"BaseUrl/balance/balance-movement";
-    public const string OnHoldAmountUri = @"BaseUrl/balance/on-hold-amount";
+    public const string BalanceUri = @"http://api.dev.subsbase.xyz/balance";
+    public const string BalanceMovementUri = @"http://api.dev.subsbase.xyz/balance/balance-movement";
+    public const string OnHoldAmountUri = @"http://api.dev.subsbase.xyz/balance/on-hold-amount";
 
 }
