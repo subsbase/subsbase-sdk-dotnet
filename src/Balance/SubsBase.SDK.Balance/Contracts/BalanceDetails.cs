5cfb2c20b0b3d224d88968df5904db10a513f702
Mohamed Shaker <m.elzantot@gmail.com>
2024-06-12 12:52:11 +0300

    balance nuget file added

--- src/Balance/SubsBase.SDK.Balance/Contracts/BalanceDetails.cs
+++ src/Balance/SubsBase.SDK.Balance/Contracts/BalanceDetails.cs
@@ -3,9 +3,8 @@ namespace SubsBase.SDK.Balance.Contracts;
 public class BalanceDetails
 {
     public Guid Id { get; set; }
-    public decimal Amount { get; set; }
+    public decimal Total { get; set; }
     public DateTime? Expiry { get; set; }
-    
     public Guid BalanceId { get; set; }
     public BalanceInfo? BalanceInfo { get; set; }
 }
--- src/Balance/SubsBase.SDK.Balance/Contracts/BalanceInfo.cs
+++ src/Balance/SubsBase.SDK.Balance/Contracts/BalanceInfo.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace SubsBase.SDK.Balance.Contracts;
 
 public class BalanceInfo
@@ -5,20 +7,26 @@ public class BalanceInfo
     public Guid Id { get; set; }
     public string Unit { get; set; } = string.Empty;
     public Dictionary<string, object>? Metadata { get; set; }
-    
-    public List<BalanceMovement>? BalanceMovements { get; set; }
     public List<BalanceDetails>? BalanceDetails { get; set; }
-    public List<BalanceHold>? BalanceHolds { get; set; }
 }
 
 public class BalanceInfoNew
 {
-    public string Unit { get; set; } = string.Empty;
-    public Dictionary<string, object>? Metadata { get; set; }
+    [JsonPropertyName("allowTotalBalanceToBeNegative")] public bool AllowTotalBalanceToBeNegative { get; set; } = false;
+    [JsonPropertyName("metadata")] public Dictionary<string, string>? Metadata { get; set; }
+    [JsonPropertyName("unit")] public string Unit { get; set; } = string.Empty;
 }
 
 
 public class BalanceInfoUpdate
 {
-    public Dictionary<string, object>? Metadata { get; set; }
+    [JsonPropertyName("metadata")] public Dictionary<string, object>? Metadata { get; set; }
+}
+
+public class BalanceSummary
+{
+    [JsonPropertyName("balanceId")] public Guid BalanceId { get; set; }
+    [JsonPropertyName("totalAmount")] public decimal TotalAmount { get; set; }
+    [JsonPropertyName("onHoldAmount")] public decimal OnHoldAmount { get; set; }
+    [JsonPropertyName("availableAmount")] public decimal AvailableAmount { get; set; }
 }
