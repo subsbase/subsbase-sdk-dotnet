84cb6b92102f17d75f7925f8ab2d86410a7936bd
Mohamed Shaker <m.elzantot@gmail.com>
2024-08-22 15:49:41 +0300

    balance sdk test has been finalized

--- src/Balance/SubsBase.SDK.Balance.Test/BalanceTests.cs
+++ src/Balance/SubsBase.SDK.Balance.Test/BalanceTests.cs
@@ -1,5 +1,5 @@
-using SubsBase.SDK.Balance.Contracts;
 using FluentAssertions;
+using SubsBase.SDK.Balance.Contracts;
 
 namespace SubsBase.SDK.Balance.Test;
 
@@ -8,172 +8,308 @@ namespace SubsBase.SDK.Balance.Test;
 [SingleThreaded]
 public class BalanceTests
 {
-    private static readonly string _publicKey = "F68B22FC-E444-4296-B415-D22434DD8061";
-    private static readonly string _privateKey = "nDCH601zDitO6gNRnDXhYLu0NN99CJbg";
-    private static Guid _balanceId;
+    private static readonly string _publicKey = "publicKey";
+    private static readonly string _privateKey = "privateKey";
+    private static Guid _1StBalanceId;
+    private static Guid _2NdBalanceId;
     private static string _releasedAmountId;
     private static string _capturedAmountId;
+    private static BalanceSdk balanceSdk;
 
+    [OneTimeSetUp]
+    public void OneTimeInit()
+    {
+        balanceSdk = new BalanceSdk(_publicKey, _privateKey, environment: Environment.Development);
+    }
 
     [Test, Order(1)]
     public void Step_01_AddNewBalanceWithoutMetaData_ShouldCreateNewBalance()
     {
-        var balanceSdk = new BalanceSdk(_publicKey, _privateKey);
         var balance = balanceSdk.Balance.CreateAsync(new BalanceInfoNew()
         {
             Unit = "EGP",
             AllowTotalBalanceToBeNegative = false,
+            Metadata = new Dictionary<string, string>()
+            {
+                {"customerId" , "subsbase_customer1"},
+                {"age", "25"}
+            },
         }).GetAwaiter().GetResult();
-
+    
         balance.IsSuccess.Should().BeTrue();
         balance.Value.Should().NotBeNull();
-        //_balanceId = balance.Value!.BalanceId;
+        _1StBalanceId = balance.Value!.BalanceId;
     }
-
-    [Test, Order(1)]
-    public void Step_01_AddNewBalance_ShouldCreateNewBalance()
+    
+    [Test, Order(2)]
+    public void Step_02_AddNewBalance_ShouldCreateNewBalance()
     {
-        var balanceSdk = new BalanceSdk(_publicKey, _privateKey);
         var balance = balanceSdk.Balance.CreateAsync(new BalanceInfoNew()
         {
             Unit = "EGP",
             Metadata = new Dictionary<string, string>()
             {
-                {"customerId" , "shaker_customer"},
-                {"age", "25"}
+                {"customerId" , "subsbase_customer2"},
+                {"age", "35"}
             },
             AllowTotalBalanceToBeNegative = false,
         }).GetAwaiter().GetResult();
-
+    
         balance.IsSuccess.Should().BeTrue();
         balance.Value.Should().NotBeNull();
-        _balanceId = balance.Value!.BalanceId;
+        _2NdBalanceId = balance.Value!.BalanceId;
     }
-
-    [Test, Order(2)]
-    public void Step_02_AddCreditBalanceMovement_ShouldLoadBalanceWithSpecificAmount()
+    
+    [Test, Order(3)]
+    public void Step_03_AddCreditBalanceMovement_ShouldLoadBalanceWithSpecificAmount()
     {
-        var balanceSdk = new BalanceSdk(_publicKey, _privateKey);
         var balance = balanceSdk.BalanceMovement.CreateAsync(new BalanceMovementNew()
         {
-            BalanceId = _balanceId,
+            BalanceId = _1StBalanceId,
             Type = MovementType.Credit,
             Amount = 1000,
             Description = "Load Balance With 1000 EGP",
         }).GetAwaiter().GetResult();
-
+    
         balance.IsSuccess.Should().BeTrue();
         balance.Value.Should().NotBeNull();
-        balance.Value!.AvailableAmount = 1000.0M;
-        balance.Value.TotalAmount = 1000.0M;
-        balance.Value.OnHoldAmount = 0.0M;
+        balance.Value!.AvailableAmount.Should().Be(1000.0M);
+        balance.Value.TotalAmount.Should().Be(1000.0M);
+        balance.Value.OnHoldAmount.Should().Be(0.0M);
+    }
+    
+    [Test, Order(4)]
+    public void Step_04_HoldBalanceAmountToBeCaptured_ShouldHoldAmount()
+    {
+        var balance = balanceSdk.OnHoldAmount.CreateAsync(new HoldAmountNew()
+        {
+            BalanceId = _1StBalanceId,
+            Amount = 1000,
+            Description = "Hold 1000 EGP",
+            ReleaseDate = DateTime.UtcNow.AddSeconds(10)
+        }).GetAwaiter().GetResult();
+    
+        balance.IsSuccess.Should().BeTrue();
+        balance.Value.Should().NotBeNull();
+        balance.Value!.OnHoldAmountId.Should().NotBeNullOrWhiteSpace();
+        _capturedAmountId = balance.Value!.OnHoldAmountId;
+    }
+    
+    [Test, Order(5)]
+    public void Step_05_CaptureOnHoldAmount_ShouldCaptureOnHoldAmount()
+    {
+        var balance = balanceSdk.OnHoldAmount.DeleteAsync(_capturedAmountId, isCaptured: true).GetAwaiter().GetResult();
+    
+        balance.IsSuccess.Should().BeTrue();
+        balance.Value.Should().NotBeNull();
+        balance.Value!.AvailableAmount.Should().Be(0.0M);
+        balance.Value.TotalAmount.Should().Be(0.0M);
+        balance.Value.OnHoldAmount.Should().Be(0.0M);
+    }
+    
+    [Test, Order(6)]
+    public void Step_06_AddCreditBalanceMovement_ShouldLoadBalanceWithSpecificAmount()
+    {
+        var balance = balanceSdk.BalanceMovement.CreateAsync(new BalanceMovementNew()
+        {
+            BalanceId = _1StBalanceId,
+            Type = MovementType.Credit,
+            Amount = 1000,
+            Description = "Load Balance With 1000 EGP",
+        }).GetAwaiter().GetResult();
+    
+        balance.IsSuccess.Should().BeTrue();
+        balance.Value.Should().NotBeNull();
+        balance.Value!.AvailableAmount.Should().Be(1000.0M);
+        balance.Value.TotalAmount.Should().Be(1000.0M);
+        balance.Value.OnHoldAmount.Should().Be(0.0M);
     }
     
     
-    [Test, Order(3)]
-    public void Step_03_AddDebitBalanceMovementWithAmountLargerThanAvailable_ShouldReturnErrorInsufficientFund()
+    
+    [Test, Order(7)]
+    public void Step_07_AddDebitBalanceMovementWithAmountLargerThanAvailable_ShouldReturnErrorInsufficientFund()
     {
-        var balanceSdk = new BalanceSdk(_publicKey, _privateKey);
         var balance = balanceSdk.BalanceMovement.CreateAsync(new BalanceMovementNew()
         {
-            BalanceId = _balanceId,
+            BalanceId = _1StBalanceId,
             Type = MovementType.Debit,
             Amount = 1500,
             Description = "Unload Balance With 1500 EGP",
         }).GetAwaiter().GetResult();
-
+    
         balance.IsSuccess.Should().BeFalse();
         balance.Value.Should().BeNull();
+        balance.Message.Should().NotBeNullOrWhiteSpace();
     }
     
-    [Test, Order(4)]
-    public void Step_04_AddDebitBalanceMovementWithAmountLessThanAvailable_ShouldUnloadBalanceWithSpecificAmount()
+    [Test, Order(8)]
+    public void Step_08_AddDebitBalanceMovementWithAmountLessThanAvailable_ShouldUnloadBalanceWithSpecificAmount()
     {
-        var balanceSdk = new BalanceSdk(_publicKey, _privateKey);
         var balance = balanceSdk.BalanceMovement.CreateAsync(new BalanceMovementNew()
         {
-            BalanceId = _balanceId,
+            BalanceId = _1StBalanceId,
             Type = MovementType.Debit,
             Amount = 500,
             Description = "Unload Balance With 500 EGP",
         }).GetAwaiter().GetResult();
-
+    
         balance.IsSuccess.Should().BeTrue();
         balance.Value.Should().NotBeNull();
-        balance.Value!.AvailableAmount = 500.0M;
-        balance.Value.TotalAmount = 500.0M;
-        balance.Value.OnHoldAmount = 0.0M;
+        balance.Value!.AvailableAmount.Should().Be(500.0M);
+        balance.Value!.TotalAmount.Should().Be(500.0M);
+        balance.Value!.OnHoldAmount.Should().Be(0.0M);
     }
     
-    [Test, Order(5)]
-    public void Step_05_HoldBalanceAmount_ShouldHoldAmount()
+    [Test, Order(9)]
+    public void Step_09_HoldBalanceAmount_ShouldHoldAmount()
     {
-        var balanceSdk = new BalanceSdk(_publicKey, _privateKey);
         var balance = balanceSdk.OnHoldAmount.CreateAsync(new HoldAmountNew()
         {
-            BalanceId = _balanceId,
+            BalanceId = _1StBalanceId,
             Amount = 200,
             Description = "Hold 200 EGP",
         }).GetAwaiter().GetResult();
-
+    
         balance.IsSuccess.Should().BeTrue();
         balance.Value.Should().NotBeNull();
         balance.Value!.OnHoldAmountId.Should().NotBeNullOrWhiteSpace();
         _releasedAmountId = balance.Value!.OnHoldAmountId;
     }
     
-    [Test, Order(6)]
-    public void Step_06_ReleaseOnHoldAmount_ShouldReleaseOnHoldAmount()
+    [Test, Order(10)]
+    public void Step_10_GetOnHoldAmountWithId_ShouldGetOnHoldAmountDetails()
     {
-        var balanceSdk = new BalanceSdk(_publicKey, _privateKey);
+        var onHoldAmountResult = balanceSdk.OnHoldAmount.GetAsync(_releasedAmountId).GetAwaiter().GetResult();
+    
+        onHoldAmountResult.IsSuccess.Should().BeTrue();
+        onHoldAmountResult.Value.Should().NotBeNull();
+        onHoldAmountResult.Value!.BalanceId.Should().Be(_1StBalanceId);
+        onHoldAmountResult.Value!.Amount.Should().Be(200.0M);
+    }
+    
+    
+    [Test, Order(10)]
+    public void Step_10_ReleaseOnHoldAmount_ShouldReleaseOnHoldAmount()
+    {
         var balance = balanceSdk.OnHoldAmount.DeleteAsync(_releasedAmountId, isCaptured: false).GetAwaiter().GetResult();
-
+    
         balance.IsSuccess.Should().BeTrue();
         balance.Value.Should().NotBeNull();
-        balance.Value!.AvailableAmount = 500.0M;
-        balance.Value.TotalAmount = 500.0M;
-        balance.Value.OnHoldAmount = 0.0M;
-
+        balance.Value!.AvailableAmount.Should().Be(500.0M);
+        balance.Value!.TotalAmount.Should().Be(500.0M);
+        balance.Value!.OnHoldAmount.Should().Be(0.0M);
+    
     }
-    [Test, Order(7)]
-    public void Step_07_HoldBalanceAmountWithExpirationDate_ShouldHoldAmount()
+    
+    [Test, Order(11)]
+    public void Step_11_HoldBalanceAmountToBeCaptured_ShouldHoldAmount()
     {
-        var balanceSdk = new BalanceSdk(_publicKey, _privateKey);
         var balance = balanceSdk.OnHoldAmount.CreateAsync(new HoldAmountNew()
         {
-            BalanceId = _balanceId,
+            BalanceId = _1StBalanceId,
             Amount = 200,
             Description = "Hold 200 EGP",
-            ExpirationDate = DateTime.UtcNow.AddDays(1)
+            ReleaseDate = DateTime.UtcNow.AddHours(1)
         }).GetAwaiter().GetResult();
-
+    
+        balance.IsSuccess.Should().BeTrue();
+        balance.Value.Should().NotBeNull();
+        balance.Value!.OnHoldAmountId.Should().NotBeNullOrWhiteSpace();
+        _capturedAmountId = balance.Value!.OnHoldAmountId;
+    }
+    
+    [Test, Order(12)]
+    public void Step_12_CaptureOnHoldAmount_ShouldCaptureOnHoldAmount()
+    {
+        var balance = balanceSdk.OnHoldAmount.DeleteAsync(_capturedAmountId, isCaptured: true).GetAwaiter().GetResult();
+    
+        balance.IsSuccess.Should().BeTrue();
+        balance.Value.Should().NotBeNull();
+        balance.Value!.AvailableAmount.Should().Be(300.0M);
+        balance.Value!.TotalAmount.Should().Be(300.0M);
+        balance.Value!.OnHoldAmount.Should().Be(0.0M);
+    }
+    
+    [Test, Order(13)]
+    public void Step_13_HoldBalanceAmountWithExpirationDate_ShouldHoldAmount()
+    {
+        var balance = balanceSdk.OnHoldAmount.CreateAsync(new HoldAmountNew()
+        {
+            BalanceId = _1StBalanceId,
+            Amount = 200,
+            Description = "Hold 200 EGP",
+            ReleaseDate = DateTime.UtcNow.AddDays(1)
+        }).GetAwaiter().GetResult();
+    
         balance.IsSuccess.Should().BeTrue();
         balance.Value.Should().NotBeNull();
         balance.Value!.OnHoldAmountId.Should().NotBeNullOrWhiteSpace();
         _releasedAmountId = balance.Value!.OnHoldAmountId;
+    
+        var onHoldAmount = balanceSdk.OnHoldAmount.GetAsync(balance.Value!.OnHoldAmountId).GetAwaiter().GetResult();
+        onHoldAmount.IsSuccess.Should().BeTrue();
+        onHoldAmount.Value.Should().NotBeNull();
+        onHoldAmount.Value.Amount.Should().Be(expected: 200.0M);
+        onHoldAmount.Value.ReleaseDate.Should().NotBeNull();
+        onHoldAmount.Value.ReleaseDate.Value.Date.Should().Be(DateTime.UtcNow.AddDays(1).Date);
     }
     
-    [Test, Order(8)]
-    public void Step_08_AddCreditBalanceMovementWithExpirationDate_ShouldLoadBalanceWithSpecificAmount()
+    
+    [Test, Order(14)]
+    public void Step_14_AddCreditBalanceMovementWithExpirationDate_ShouldLoadBalanceWithSpecificAmount()
     {
-        var balanceSdk = new BalanceSdk(_publicKey, _privateKey);
         var balance = balanceSdk.BalanceMovement.CreateAsync(new BalanceMovementNew()
         {
-            BalanceId = _balanceId,
+            BalanceId = _1StBalanceId,
             Type = MovementType.Credit,
             Amount = 1000,
             Description = "Load Balance With 1000 EGP",
-            ExpirationDate = DateTime.UtcNow.AddDays(30)
+            ExpirationDate = DateTime.UtcNow.AddSeconds(5)
         }).GetAwaiter().GetResult();
-
+    
         balance.IsSuccess.Should().BeTrue();
         balance.Value.Should().NotBeNull();
-        balance.Value!.AvailableAmount = 1300.0M;
-        balance.Value.TotalAmount = 1500.0M;
-        balance.Value.OnHoldAmount = 200.0M;
+        balance.Value!.AvailableAmount.Should().Be(1100.0M);
+        balance.Value!.TotalAmount.Should().Be(1300.0M);
+        balance.Value!.OnHoldAmount.Should().Be(200.0M);
+        
     }
-
-
-
-
+    
+    [Test, Order(15)]
+    public void Step_15_GetBalanceDetails_ShouldGetAllBalanceDetails()
+    {
+        Thread.Sleep(20 * 1000);
+        var balance = balanceSdk.Balance.GetAsync(_1StBalanceId).GetAwaiter().GetResult();
+        balance.Should().NotBeNull();
+        balance.IsSuccess.Should().BeTrue();
+        balance.Value!.BalanceSummary.AvailableAmount.Should().Be(100.0M);
+        balance.Value!.BalanceSummary.TotalAmount.Should().Be(300.0M);
+        balance.Value!.BalanceSummary.OnHoldAmount.Should().Be(200.0M);
+    }
+    
+    [Test, Order(16)]
+    public void Step_16_GetAllBalancesDetails_ShouldGetAllBalances()
+    {
+        var balance = balanceSdk.Balance.GetAllAsync(new List<string>{_1StBalanceId.ToString(),_2NdBalanceId.ToString()}).GetAwaiter().GetResult();
+        balance.Should().NotBeNull();
+        balance.IsSuccess.Should().BeTrue();
+        balance.Value.Count.Should().Be(expected: 2);
+        balance.Value[0].BalanceId.Should().Be(_1StBalanceId);
+        balance.Value[0].AvailableAmount.Should().BePositive();
+        balance.Value[0].OnHoldAmount.Should().BePositive();
+        balance.Value[0].Unit.Should().NotBeNullOrEmpty();
+    }
+    
+    [Test, Order(17)]
+    public void Step_17_GetAllBalancesOnHoldAmountsDetails_ShouldGetAllOnHoldAmountsDetails()
+    {
+        var balance = balanceSdk.OnHoldAmount.GetBalanceOnHoldAmountsAsync(_1StBalanceId).GetAwaiter().GetResult();
+        balance.Should().NotBeNull();
+        balance.IsSuccess.Should().BeTrue();
+        balance.Value.OnHoldAmounts.Should().NotBeEmpty();
+        balance.Value.BalanceId.Should().Be(_1StBalanceId);
+        balance.Value.OnHoldAmounts.Sum(x => x.Amount).Should().Be(200);
+    }
+    
 }
