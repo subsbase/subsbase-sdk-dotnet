93e76263081346eb00c0f9576b259284b5eae3fe
Mohamed Shaker <58594920+mElzantot@users.noreply.github.com>
2024-09-29 11:42:29 +0300

    utcTimestamp has been added to balance movement create api (#5)

--- src/Balance/SubsBase.SDK.Balance/Contracts/BalanceDetails.cs
+++ src/Balance/SubsBase.SDK.Balance/Contracts/BalanceDetails.cs
@@ -16,6 +16,7 @@ public class BalanceDetails
 public class BalanceMovement
 {
     [JsonPropertyName("utcTimestamp")] public DateTime UtcTimestamp { get; set; }
+    [JsonPropertyName("createdAtUtc")] public DateTime CreatedAtUtc { get; set; }
     [JsonPropertyName("amount")] public decimal Amount { get; set; }
     [JsonPropertyName("netBalance")] public decimal NetBalance { get; set; }
     [JsonPropertyName("description")] public string Description { get; set; } = string.Empty;
