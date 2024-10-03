34bf366e1ceb5d27cebad0cdb23cdb39e79c2015
Mohamed Shaker <58594920+mElzantot@users.noreply.github.com>
2024-10-03 14:00:07 +0300

    feature/add optional environment base url when initialize sdk in live mode (#6)
    * utcTimestamp has been added to balance movement create api
    
    * optional prod url has been added when init sdk
    
    * version 1.5.3

--- src/Balance/SubsBase.SDK.Balance/Constants.cs
+++ src/Balance/SubsBase.SDK.Balance/Constants.cs
@@ -7,29 +7,19 @@ public class IConfigurationConstants
     public string OnHoldAmountUri;
 }
 
-public class DevelopmentConstants : IConfigurationConstants
+public class ConfigurationConstants : IConfigurationConstants
 {
-    private const string DefaultBaseUrl = "http://api.dev.subsbase.xyz";
+    private const string DevBaseUrl = "http://api.dev.subsbase.xyz";
+    private const string ProdBaseUrl = "http://api.subsbase.io";
 
-    public DevelopmentConstants(string? baseUrl = null)
+    public ConfigurationConstants(Environment environment, string? baseUrl = null)
     {
-        if (string.IsNullOrEmpty(baseUrl))
-        {
-            baseUrl = DefaultBaseUrl;
-        }
+        baseUrl = !string.IsNullOrEmpty(baseUrl) ? baseUrl.TrimEnd().Trim('/') :
+            environment == Environment.Development ? DevBaseUrl : ProdBaseUrl;
         
         BalanceUri = $"{baseUrl}/balance/";
         BalanceMovementUri = $"{baseUrl}/balance/balance-movement/";
         OnHoldAmountUri = $"{baseUrl}/balance/on-hold-amount/";
     }
-}
 
-public class ProductionConstants : IConfigurationConstants
-{
-    public ProductionConstants()
-    {
-        BalanceUri = "http://api.subsbase.io/balance/";
-        BalanceMovementUri = "http://api.subsbase.io/balance/balance-movement/";
-        OnHoldAmountUri = "http://api.subsbase.io/balance/on-hold-amount/";
-    }
 }
